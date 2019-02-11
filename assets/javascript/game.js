3112a283468da59f8475fee1ecaedc63f68d16f8
chris <chrissalvado@hotmail.com>
2019-02-11 10:55:16 -0600

    we have a start

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var randomNumber ;
+var win ;
+var lost ;
+
+for(var i=0 ; i < 4; i++){
+
+    var crystals= $("<div>");
+        crystals.attr("class", 'crystal')
+
+        $(".crystals").append(crystals);
+
+
+
+}
