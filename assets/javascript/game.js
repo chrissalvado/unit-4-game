f998e433465fc8115698e24ae27194a771997f82
chris <chrissalvado@hotmail.com>
2019-02-11 12:20:59 -0600

    created the randomizer for 4 squares as well as the 19-120

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,24 @@ var randomNumber ;
 var win ;
 var lost ;
 
+var randomNumber = Math.floor(Math.random() * 101) + 19;
+console.log(randomNumber)
+
+$("#results").html('Random Result: ');
+
 for(var i=0 ; i < 4; i++){
 
+    var random = Math.floor(Math.random() * 12);
+
     var crystals= $("<div>");
-        crystals.attr("class", 'crystal')
+        crystals.attr({
+            "class": 'crystal',
+            "data-random":random
+        });
 
         $(".crystals").append(crystals);
 
 
 
 }
+
