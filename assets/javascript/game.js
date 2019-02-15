6375897e84e2d67bee36367047b2d132c97394bc
chris <chrissalvado@hotmail.com>
2019-02-15 12:48:57 -0600

    project is finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var RandomNumber;
 var win = 0;
 var lost = 0;
 var previous = 0;
-
+var imageArray= ["https://pbs.twimg.com/profile_images/378800000856607865/e96CZNLU_400x400.jpeg", "http://i.dailymail.co.uk/i/pix/2016/04/15/15/333699FA00000578-3541921-image-a-89_1460729476370.jpg", "https://image.tmdb.org/t/p/original/7zMtjSgbsgBu0BXQodHXMnRTDui.jpg","https://thefangirlsdilemma.files.wordpress.com/2012/09/billy-crystal-miracle-max.jpg"]
 var ResetAndStart = function () {
 
     $(".crystals").empty();
@@ -15,10 +15,11 @@ var ResetAndStart = function () {
 
         var random = Math.floor(Math.random() * 11) + 1;
 
-        var crystal = $("<div>");
+        var crystal = $("<img>");
         crystal.attr({
             "class": 'crystal',
-            "data-random": random
+            "data-random": random,
+            "src": imageArray[i]
 
 
 
@@ -31,7 +32,7 @@ var ResetAndStart = function () {
 
     }
 
-    $("#previous").html(previous);
+    $("#previous").html('previous '+ previous);
 }
 
 
@@ -45,18 +46,20 @@ $(document).on('click', ".crystal", function () {
 
     previous += num;
 
-    $("#previous").html(previous);
+
+    $("#previous").html('Previous ' + previous);
 
     console.log(previous, RandomNumber);
 
     if (previous > RandomNumber) {
         // debugger;
         console.log('you lose bud');
-        lost--;
+        lost++;
 
         $("#lost").html(lost);
 
         previous = 0;
+        alert("You lose!!!")
 
         ResetAndStart();
 
@@ -67,6 +70,7 @@ $(document).on('click', ".crystal", function () {
         $("#win").html(win);
 
         previous = 0;
+        alert("We have a winner!!")
 
         ResetAndStart();
 
